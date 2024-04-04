2158c190420fca175286deeb4354702c2aa2e547
Linn Ammouri <108581680+l1nn1l@users.noreply.github.com>
2024-04-04 12:37:38 +0300

    added routing to home page

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MainContentComponent } from '../main-content/main-content.component';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, MainContentComponent, SideBarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
