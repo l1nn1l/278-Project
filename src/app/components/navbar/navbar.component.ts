cde2e6e712687b025f0013c7a094d154f57f9f6f
Linn Ammouri <108581680+l1nn1l@users.noreply.github.com>
2024-04-09 08:39:57 +0300

    created frontend for advanced search feature in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DropdownModule, ReactiveFormsModule, InputTextModule, RadioButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  showModal: boolean = false;
+
+  constructor() { }
+
+  toggleModal(): void {
+    this.showModal = !this.showModal;
+    console.log('toggleModal called', this.showModal);
+  }
+
+  onBackgroundClick(event: MouseEvent): void {
+    this.showModal = false;
+  }
+
+  formGroup = new FormGroup({
+    type: new FormControl('any'),
+    owner: new FormControl('anyone'),
+    includes_words: new FormControl(''),
+    item_name: new FormControl(''),
+    location: new FormControl('anywhere'),
+    fileStatus: new FormControl('')
+  });
+
+  types = [
+    { label: 'Any', value: 'any' },
+    // Add more types as needed
+  ];
+
+  owners = [
+    { label: 'Anyone', value: 'anyone' },
+    { label: 'Owned by me', value: 'owned-by-me' },
+    { label: 'Not owned by me', value: 'not-owned-by-me' },
+    // Add more owner options as needed
+  ];
+
+  driveLocations = [
+    { label: 'Anywhere', value: 'anywhere' },
+    { label: 'My Drive', value: 'my-drive' },
+    { label: 'Shared with me', value: 'shared-with-me' },
+  ]
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+    this.toggleModal();
+  }
 
 }
