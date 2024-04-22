290b65b3fb7f9b8db540a42f12386b5d6ec51367
Linn Ammouri <108581680+l1nn1l@users.noreply.github.com>
2024-04-22 22:02:49 +0300

    merged changes from RawanBranch to master

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { MyDriveComponent } from './components/my-drive/my-drive.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 export const routes: Routes = [
-    {path: 'home', component: MainPageComponent},
+    {path: 'home', component: MainContentComponent},
+    { path: 'My-Drive', component: MyDriveComponent},
+    {path: 'Shared-w-me', component: MyDriveComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    // { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
